a20f66433feb390aa13de31f9bc39339145a9234
Michael Tolmie <michaeltolmie@gmail.com>
2017-05-05 19:35:04 -0700

    Add files via upload

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,42 @@
+var chosenNumber = Math.floor(Math.random() * ((120 - 19) + 1) + 19);
+$("#number-to-guess").text(chosenNumber);
 
+    var counter = 0;
+    var winCounter = 0;
+    var lossCounter = 0;
 
-// $("#start").click(startClickRedCrystal);
+var crystalNum = new Array(4);
+var m = 0;
+var min = 1;
+var max = 12;
+
+while (m < crystalNum.length) {
+    var randnum = Math.floor(Math.random() * (max - min + 1) + min);
+
+    var found = false;
+
+    for (var i = 0; i < crystalNum.length; i++) {
+        if (crystalNum[i] == randnum) {
+            found = true;
+            break;
+        }
+    }
+
+    if (!found) {
+        crystalNum[m] = randnum;
+        m++;
+    }
+}
+console.log(crystalNum);
 
 	var crystalOptions = ["blueCrystal", "greenCrystal", "yellowCrystal", "redCrystal"];
 
     // Computer selected solution will be held here.
     var chosenNumber = Math.floor(Math.random() * ((120-19)+1) + 19);
-   	console.log("chosenNumber", chosenNumber);
+   console.log("chosenNumber", chosenNumber);
 
     // This will break the numbers within the crystal within the solution
-    //var numbersInChosenCrystal = [];
-
+    var numbersInChosenCrystal = [];
     // This will be the numbers
     var numbers = 0;
 
@@ -21,11 +47,6 @@
 
     // Holds the numbers guessed
     var numbersHeld;
-
-    // Game counters
-    var winCounter = 0;
-    var loseCounter = 0;
-    var guessCounter = 5;
 
     // crystal variables for counting each number on each cystal
 
@@ -53,42 +74,77 @@ function selectYellowCrystalNumber() {
 var redCrystalNumber = Math.floor(Math.random() * ((12-1)+1) + 1);
    console.log("yellowCrystalNumber", redCrystalNumber);
 }
-//TODO: 1 -write a variable with the name "counter", it should be in the global scope
 
-//TODO: 2- Funnction A: We need a function that will add the total of the values inside of any clicked crystals, and
-	//TODO:Store the running sum of these numbers inside a variable/counter
-	//TODO: Return that sum
-
-//TODO: 3- Funciton B: We need another function here to compare the running total in the counter (returned form the funciton above) to the computer generated chosenNumber online 8 above
-	//TODO: We need a conditional statement here - if the running total is equal to the chosenNumber, tell the user that they have won the game, then restart the game (hint: by calling the startGame function)
-	//TODO: else if the running total is greater than chosenNumber, tell the user they have lost and restart the game (hint: by calling the startGame function)
-	//TODO: else, the game continues as before and the user can click on more crystals
-
-//TODO: HINT: Remember to use console.logs generously to check everything as you go! 
+$("#red-crystal").attr("data-crystalvalue", crystalNum[0])
+$("#green-crystal").attr("data-crystalvalue", crystalNum[1])
+$("#blue-crystal").attr("data-crystalvalue", crystalNum[2])
+$("#black-crystal").attr("data-crystalvalue", crystalNum[3])
 
 $(document).ready(function(){
-
+    console.log("document.ready");
     $("#redCrystal").on("click", function() {
         selectRedCrystalNumber();
+if (counter === chosenNumber) {
+        winCounter++
+        $("#wins").text(winCounter);
+
+    } else if (counter >= chosenNumber) {
+        lossCounter++
+        $("#losses").text(lossCounter);
+    }
+    $("#current-total").text(counter);
+
+});
+
+    $("#redCrystal").on("click", function() {
+    var crystalValue = ($(this).attr("crystalvalue"));
+    crystalValue = parseInt(crystalValue);
+    counter += crystalValue;
+
+    $("#current-total").text(counter);
+
 	});
 
     $("#blueCrystal").on("click", function() {
         selectBlueCrystalNumber();
      });
-
+   
     $("#greenCrystal").on("click", function() {
         selectGreenCrystalNumber();
     });
-
+   
     $("#yellowCrystal").on("click", function() {
         selectYellowCrystalNumber();
     });
 
+
 	$("#randomButton").on("click",function(){
 		alert("test");
 	});
-
 });
+
+//TODO: 1 -write a variable with the name "counter", it should be in the global scope
+ 
+ //TODO: 2- Funnction A:  need a function that will add the total of the values inside of any clicked crystals, and
+  //TODO:Store the running sum of these numbers inside a variable/counter
+  //TODO: Return that sum
+ 
+ //TODO: 3- Funciton B:  need another function here to compare the running total in the counter (returned form the funciton above) to the computer generated chosenNumber online 8 above
+ //TODO: We need a conditional statement here - if the running total is equal to the chosenNumber, tell the user that they have won the game, then restart the game (hint: by calling the startGame function)
+  //TODO: else if the running total is greater than chosenNumber, tell the user they have lost and restart the game (hint: by calling the startGame function)
+  //TODO: else, the game continues as before and the user can click on more crystals
+  
+//
+
+
+
+
+
+
+
+
+
+
 
 // var foo = [];
 // for (var i = n; n <= j; i++) {
@@ -103,13 +159,13 @@ $(document).ready(function(){
 
     // startGame()
     // It's how we we will start and restart the game.
+   
+ 
 
 
 
 
-
-
-
+   
 
 //     function startGame() {
 //         guessCounter = 5;
@@ -150,3 +206,6 @@ $(document).ready(function(){
 // }
 
 // };
+
+
+	
