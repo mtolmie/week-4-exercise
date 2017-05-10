7320e5bfd1ca622ab024a127033951c298632872
Michael Tolmie <michaeltolmie@gmail.com>
2017-05-09 17:04:25 -0700

    Add files via upload

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,5 @@
 var chosenNumber = Math.floor(Math.random() * ((120 - 19) + 1) + 19);
+// You need to add a <span> with the ID of number-to-guess so that this will show up - I went ahead and added it in the HTML
 $("#number-to-guess").text(chosenNumber);
 
     var counter = 0;
@@ -28,56 +29,96 @@ while (m < crystalNum.length) {
     }
 }
 
- counter = 0;
+ // counter = 0;
     var winCounter = 0;
     var lossCounter = 0;
-// console.log(crystalNum);
+ console.log(crystalNum);
+ console.log(chosenNumber)
 
-	var crystalOptions = ["blueCrystal", "greenCrystal", "yellowCrystal", "redCrystal"];
+//I added this here so it could be called later.
+ function init() {
+        chosenNumber = Math.floor(Math.random() * ((120 - 19) + 1) + 19);
 
-    // Computer selected solution will be held here.
-    var chosenNumber = Math.floor(Math.random() * ((120-19)+1) + 19);
-   console.log("chosenNumber", chosenNumber);
+       $("#number-to-guess").text(chosenNumber);
 
-    // This will break the numbers within the crystal within the solution
-    var numbersInChosenCrystal = [];
-    // This will be the numbers
-    var numbers = 0;
+       counter = 0;
 
-    var ghostRider = [];
 
-    // Holds all of the wrong guesses.
-    var wrongGuesses = [];
+       var crystalNum = new Array(4);
+        var j = 0;
+        var min = 1;
+        var max = 12;
 
-    // Holds the numbers guessed
-    var numbersHeld;
+       while (j < crystalNum.length) {
+            var randnum = Math.floor(Math.random() * (max - min + 1) + min);
+
+           var found = false;
+
+           for (var i = 0; i < crystalNum.length; i++) {
+                if (crystalNum[i] == randnum) {
+                    found = true;
+                    break;
+                }
+            }
+
+           if (!found) {
+                crystalNum[j] = randnum;
+                j++;
+            }
+        }
+                console.log(crystalNum);
+
+
+        $("#red-crystal").attr("data-crystalvalue", crystalNum[0])
+        $("#green-crystal").attr("data-crystalvalue", crystalNum[1])
+        $("#blue-crystal").attr("data-crystalvalue", crystalNum[2])
+        $("#black-crystal").attr("data-crystalvalue", crystalNum[3])
+}
+    // var crystalOptions = ["blueCrystal", "greenCrystal", "yellowCrystal", "redCrystal"];
+
+ //    // Computer selected solution will be held here.
+ //    var chosenNumber = Math.floor(Math.random() * ((120-19)+1) + 19);
+ //   console.log("chosenNumber", chosenNumber);
+
+ //    // This will break the numbers within the crystal within the solution
+ //    var numbersInChosenCrystal = [];
+ //    // This will be the numbers
+ //    var numbers = 0;
+
+ //    var ghostRider = [];
+
+ //    // Holds all of the wrong guesses.
+ //    var wrongGuesses = [];
+
+ //    // Holds the numbers guessed
+ //    var numbersHeld;
 
     // crystal variables for counting each number on each cystal
 
-    var redCrystal = 0;
-    var blueCrystal = 0;
-    var yellowCrystal = 0;
-    var greenCrystal = 0;
+//     var redCrystal = 0;
+//     var blueCrystal = 0;
+//     var yellowCrystal = 0;
+//     var greenCrystal = 0;
 
-function selectRedCrystalNumber() {
-var redCrystalNumber = Math.floor(Math.random() * ((12-1)+1) + 1);
-   console.log("redCrystalNumber", redCrystalNumber);
-}
+// function selectRedCrystalNumber() {
+// var redCrystalNumber = Math.floor(Math.random() * ((12-1)+1) + 1);
+//    console.log("redCrystalNumber", redCrystalNumber);
+// }
 
-function selectBlueCrystalNumber() {
-var redCrystalNumber = Math.floor(Math.random() * ((12-1)+1) + 1);
-   console.log("blueCrystalNumber", redCrystalNumber);
-}
+// function selectBlueCrystalNumber() {
+// var redCrystalNumber = Math.floor(Math.random() * ((12-1)+1) + 1);
+//    console.log("blueCrystalNumber", redCrystalNumber);
+// }
 
-function selectGreenCrystalNumber() {
-var redCrystalNumber = Math.floor(Math.random() * ((12-1)+1) + 1);
-   console.log("greenCrystalNumber", redCrystalNumber);
-}
+// function selectGreenCrystalNumber() {
+// var redCrystalNumber = Math.floor(Math.random() * ((12-1)+1) + 1);
+//    console.log("greenCrystalNumber", redCrystalNumber);
+// }
 
-function selectYellowCrystalNumber() {
-var redCrystalNumber = Math.floor(Math.random() * ((12-1)+1) + 1);
-   console.log("yellowCrystalNumber", redCrystalNumber);
-}
+// function selectYellowCrystalNumber() {
+// var redCrystalNumber = Math.floor(Math.random() * ((12-1)+1) + 1);
+//    console.log("yellowCrystalNumber", redCrystalNumber);
+// }
 
 $("#redCrystal").attr("crystalvalue", crystalNum[0])
 $("#blueCrystal").attr("crystalvalue", crystalNum[1])
@@ -91,9 +132,21 @@ $("#redCrystal").on("click", function() {
     console.log(crystalValue);
     crystalValue = parseInt(crystalValue);
     counter += crystalValue;
-    
+ // I added this from the code below to get the win/loss counters to work    
+     if (counter === chosenNumber) {
+        winCounter++
+        $("#wins").text(winCounter + "");
+// I added the init function to reset the numbers and start again    
+        init();
+
+    } else if (counter > chosenNumber) {
+        lossCounter++
+        $("#losses").text(lossCounter);
+// I added the init function to reset the numbers and start again    
+        init();
+    }
+ //..........................................................................................    
     $("#current-total").text(counter);
-   
 
     });
 
@@ -102,81 +155,140 @@ $("#redCrystal").on("click", function() {
     var crystalValue = ($(this).attr("crystalvalue"));
     crystalValue = parseInt(crystalValue);
     counter += crystalValue;
+ // I added this from the code below to get the win/loss counters to work
+     if (counter === chosenNumber) {
+        winCounter++
+        $("#wins").text(winCounter + "");
+// I added the init function to reset the numbers and start again    
+        init();
 
+    } else if (counter > chosenNumber) {
+        lossCounter++
+        $("#losses").text(lossCounter);
+// I added the init function to reset the numbers and start again    
+        init();
+    }
+//..........................................................................................     
+    $("#current-total").text(counter + "");
+
+    });
+// I copied the code from the 2 crystal functions above and changed them to the other 2 needed crystals
+    $("#greenCrystal").on("click", function() {
+    var crystalValue = $(this).attr("crystalvalue");
+    console.log(crystalValue);
+    crystalValue = parseInt(crystalValue);
+    counter += crystalValue;
+ // I added this from the code below to get the win/loss counters to work   
+     if (counter === chosenNumber) {
+        winCounter++
+        $("#wins").text(winCounter + "");
+// I added the init function to reset the numbers and start again    
+        init();
+
+    } else if (counter > chosenNumber) {
+        lossCounter++
+        $("#losses").text(lossCounter);
+// I added the init function to reset the numbers and start again    
+        init();
+    }
+ //..........................................................................................   
+    $("#current-total").text(counter);
+   
+    });
+
+
+    $("#yellowCrystal").on("click", function() {
+    var crystalValue = ($(this).attr("crystalvalue"));
+    crystalValue = parseInt(crystalValue);
+    counter += crystalValue;
+ // I added this from the code below to get the win/loss counters to work
+    if (counter === chosenNumber) {
+        winCounter++
+        $("#wins").text(winCounter + "");
+// I added the init function to reset the numbers and start again    
+        init();
+
+    } else if (counter > chosenNumber) {
+        lossCounter++
+        $("#losses").text(lossCounter);
+// I added the init function to reset the numbers and start again    
+        init();
+    }
+//.......................................................................................... 
     $("#current-total").text(counter + "");
 
     });
 
-$(document).ready(function(){
-    console.log("document.ready");
-    $("#redCrystal").on("click", function() {
-        selectRedCrystalNumber();
-if (counter === chosenNumber) {
-        winCounter++
-        $("#wins").text(winCounter + "");
+// $(document).ready(function(){
+//     console.log("document.ready");
+//     $("#redCrystal").on("click", function() {
+//         selectRedCrystalNumber();
+// if (counter === chosenNumber) {
+//         winCounter++
+//         $("#wins").text(winCounter + "");
 
-    } else if (counter > chosenNumber) {
-        lossCounter++
-        $("#losses").text(lossCounter);
-    }
-    $("#current-total").text(counter);
+//     } else if (counter > chosenNumber) {
+//         lossCounter++
+//         $("#losses").text(lossCounter);
+//     }
+//     $("#current-total").text(counter);
 
-});
+// });
 
     
 
-    $("#blueCrystal").on("click", function() {
-        selectBlueCrystalNumber();
+//     $("#blueCrystal").on("click", function() {
+//         selectBlueCrystalNumber();
 
-        if (counter === chosenNumber) {
-        winCounter++
-        $("#wins").text(winCounter);
+//         if (counter === chosenNumber) {
+//         winCounter++
+//         $("#wins").text(winCounter);
 
-    } else if (counter > chosenNumber) {
-        lossCounter++
-        $("#losses").text(lossCounter);
-    }
-    $("#current-total").text(counter);
+//     } else if (counter > chosenNumber) {
+//         lossCounter++
+//         $("#losses").text(lossCounter);
+//     }
+//     $("#current-total").text(counter);
 
-     });
+//      });
 
    
-    $("#greenCrystal").on("click", function() {
-        selectGreenCrystalNumber();
+//     $("#greenCrystal").on("click", function() {
+//         selectGreenCrystalNumber();
 
 
-        if (counter === chosenNumber) {
-        winCounter++
-        $("#wins").text(winCounter);
+//         if (counter === chosenNumber) {
+//         winCounter++
+//         $("#wins").text(winCounter);
 
-    } else if (counter > chosenNumber) {
-        lossCounter++
-        $("#losses").text(lossCounter);
-    }
-    $("#current-total").text(counter);
+//     } else if (counter > chosenNumber) {
+//         lossCounter++
+//         $("#losses").text(lossCounter);
+//     }
+//     $("#current-total").text(counter);
 
-    });
+//     });
    
-    $("#yellowCrystal").on("click", function() {
-        selectYellowCrystalNumber();
+//     $("#yellowCrystal").on("click", function() {
+//         selectYellowCrystalNumber();
 
-        if (counter === chosenNumber) {
-        winCounter++
-        $("#wins").text(winCounter);
+//         if (counter === chosenNumber) {
+//         winCounter++
+//         $("#wins").text(winCounter);
 
-    } else if (counter > chosenNumber) {
-        lossCounter++
-        $("#losses").text(lossCounter);
-    }
-    $("#current-total").text(counter);
+//     } else if (counter > chosenNumber) {
+//         lossCounter++
+//         $("#losses").text(lossCounter);
+//     }
+//     $("#current-total").text(counter);
 
-    });
+//     });
 
 
-	$("#randomButton").on("click",function(){
-		alert("test");
-	});
-});
+//  $("#randomButton").on("click",function(){
+//      alert("test");
+//  });
+// });
 
 //TODO: 1 -write a variable with the name "counter", it should be in the global scope
  
@@ -190,6 +302,12 @@ if (counter === chosenNumber) {
   //TODO: else, the game continues as before and the user can click on more crystals
   
 //
+
+
+
+
+
+
 
 
 
